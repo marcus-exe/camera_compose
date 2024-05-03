961113b7ca50e8edd7bcbdde64ff4d8a8b862e79
Marcus Sena <mens.eai20@uea.edu.br>
2024-05-03 04:59:23 -0400

    Adding Icon and Turning Camera Position

--- app/src/main/java/br/com/graest/camera/MainActivity.kt
+++ app/src/main/java/br/com/graest/camera/MainActivity.kt
@@ -5,13 +5,19 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -36,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     LifecycleCameraController(applicationContext).apply {
                         setEnabledUseCases(
                             CameraController.IMAGE_CAPTURE or
-                            CameraController.VIDEO_CAPTURE
+                                    CameraController.VIDEO_CAPTURE
                         )
                     }
                 }
@@ -52,9 +58,24 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(padding)
                     ) {
-                        CameraPreview(controller = controller,
+                        CameraPreview(
+                            controller = controller,
                             modifier = Modifier.fillMaxSize()
+                        )
+                        IconButton (
+                            onClick = {
+                                controller.cameraSelector =
+                                    if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                                        CameraSelector.DEFAULT_FRONT_CAMERA
+                                    } else CameraSelector.DEFAULT_BACK_CAMERA
+                            },
+                            modifier = Modifier.offset(16.dp, 16.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Cameraswitch,
+                                contentDescription = "Switch Camera"
                             )
+                        }
                     }
                 }
             }
